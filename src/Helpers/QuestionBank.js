ff4c716903ca171fd493c7f5875f44885680f4af
ogunraph_lab <scitech4africa@gmail.com>
2022-08-05 06:48:49 -0700

    new files

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,27 @@
+export const Questions =[ 
+    {
+      prompt:'As soon as the state of the React component is modified, the component _______',
+      optionA:'Does nothing, you have to call the render method to make the component',  
+      optionB:' restore the component',  
+      optionC:'recreated from scratch',  
+      optionD:'None of the above', 
+      answer:'B' 
+    },
+    {
+      prompt:'which is a streaming Platform?',
+      optionA:'Twitter',  
+      optionB:'Twitch',  
+      optionC:'Google',  
+      optionD:'Robinhood', 
+      answer:'B' 
+    },
+    {
+      prompt:'Best tech youtube channel',
+      optionA:'PedroTech',  
+      optionB:'Dev Ed',  
+      optionC:'Fireship',  
+      optionD:'TechWithTim', 
+      answer:'A' 
+    },
+   
+];
